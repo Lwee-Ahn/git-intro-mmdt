1c25b250fd63799e63d623b049c77219d6e72893
Lwee-Ahn <astronaut2026@gmail.com>
2025-03-16 22:10:24 +0630

    modified weatherapi.py

--- weatherapi.py
+++ weatherapi.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import requests
 import json
+import os
 
-API_KEY='f9b73bc8d078910348d0df1a4a04a789'
-weather_url=f'https://api.openweathermap.org/data/2.5/weather?q=Yangon&appid={API_KEY}'
-weather_json=requests.get(weather_url).json()
+API_KEY=os.getEnv['API KEY']
+weather_url = f'https://api.openweathermap.org/data/2.5/weather?q=Yangon&appid={API_KEY}'
 
+# Fetch json data from weather_url
+weather_json = requests.get(weather_url).json()
+
+# Flatten the nested json file
 df_weather = pd.json_normalize(weather_json,record_path='weather',meta=[['main','temp_min'],['main','temp_max']])
 
 columns = ['description','main.temp_min','main.temp_max']
